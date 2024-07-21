c647e8a7a1a0057e078d8fb092fb8f1daeaccab4
Sam-Akbari <yarsam071@gmail.com>
2024-07-21 12:58:13 +0330

    Add files via upload

---
+++ faktoril.py
@@ -0,0 +1,6 @@
+while True:#اگر شرط درست باشد
+    a = int(input("Enter:  "))#a
+    for i in range(0, a):#بهصورت فاکتوریل نمایش میدهد
+            for j in range(0, i):#به صورت فاکتوریل نمایش میدهد
+                print(i * j)#ضرب انجام میشود
+#ناقص
